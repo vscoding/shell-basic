4a039d290fd541a01c33b837bbf560e55524263d
zhenjie zhu <devops@kubectl.net>
2024-06-24 16:13:25 +0800

    Add new shell scripts for GitLab, GitHub, and Code
    A new line was added to the existing 'basic.sh' script, and three new shell scripts were created - 'basic_gitlab.sh', 'basic_code.sh', and 'basic_github.sh'. Each new script defines the root URL for a different code hosting platform and echoes it.

---
+++ basic_github.sh
@@ -0,0 +1,3 @@
+ROOT_URI=https://raw.githubusercontent.com/iprt/build-project/main
+
+echo "root uri is $ROOT_URI"
---
+++ basic_gitlab.sh
@@ -0,0 +1,3 @@
+ROOT_URI=https://gitlab.com/iprt/build-project/-/blob/main
+
+echo "root uri is $ROOT_URI"
