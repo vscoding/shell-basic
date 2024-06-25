f8b4e59d2385b61c60d8d40c361b80d139b73419
zhenjie zhu <tech@intellij.io>
2024-06-25 18:50:29 +0800

    🔧 Added color output to HTTP status check script
    Extended the basic.sh script to output messages in different colors. The color green indicates successful HTTP status checks, while red is used to signify failures. This change enhances the visibility of these messages, making it easier to identify success or failure at a glance.

--- build-project/basic.sh
+++ build-project/basic.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # shellcheck disable=SC2086
+GREEN='\033[0;32m' # 绿色
+RED='\033[0;31m'   # 红色
+NC='\033[0m'
+
 response=$(curl -m 3 -s -o /dev/null -w "%{http_code}" https://code.kubectl.net)
 if [ $response -eq 200 ]; then
-  echo "Success: HTTP status code is 200 <https://code.kubectl.net>"
+  echo -e "${GREEN}Success: HTTP status code is 200 <https://code.kubectl.net>${NC}"
   ROOT_URI=https://code.kubectl.net/devops/build-project/raw/branch/main
 else
-  echo "Failed: HTTP status code is not 200, but $response"
+  echo -e "${RED}Failed: HTTP status code is not 200, but $response.${NC}"
   ROOT_URI=https://gitlab.com/iprt/build-project/-/raw/main
 fi
 
-echo "root uri is $ROOT_URI"
+echo -e "${GREEN}ROOT_URI is $ROOT_URI${NC}"
