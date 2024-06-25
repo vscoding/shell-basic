2593fe5fd3425a6b7ebfac6fbeaaa1064f0e5f64
zhenjie zhu <tech@intellij.io>
2024-06-25 18:51:46 +0800

    🔧 Adjusted success message punctuation
    Adjusted the punctuation in the success message for 'build-project'. The change was purely grammatical - a full stop was added at the end of the success message. This adjustment makes the output message more consistent and professional.

--- build-project/basic.sh
+++ build-project/basic.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 
 response=$(curl -m 3 -s -o /dev/null -w "%{http_code}" https://code.kubectl.net)
 if [ $response -eq 200 ]; then
-  echo -e "${GREEN}Success: HTTP status code is 200 <https://code.kubectl.net>${NC}"
+  echo -e "${GREEN}Success: HTTP status code is 200. <https://code.kubectl.net>${NC}"
   ROOT_URI=https://code.kubectl.net/devops/build-project/raw/branch/main
 else
   echo -e "${RED}Failed: HTTP status code is not 200, but $response.${NC}"
