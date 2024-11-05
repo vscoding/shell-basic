7dcb8430a395162eec1ff984634d4d95580bb3a5
zhenjie zhu <tech@intellij.io>
2024-11-06 02:59:35 +0800

    🚀 Refactor getRootUri function in basic.sh
    Encapsulated URL checking logic into a new function named `getRootUri` for better modularity and readability. Localized color variables within the function scope to avoid global pollution. This change improves code maintainability and enhances clarity.

--- build-project/basic.sh
+++ build-project/basic.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-# shellcheck disable=SC2086 disable=SC2034
-GREEN='\033[0;32m' # 绿色
-RED='\033[0;31m'   # 红色
-NC='\033[0m'
+# shellcheck disable=SC2086 disable=SC2034  disable=SC2155
+function getRootUri() {
+  local GREEN='\033[0;32m' # 绿色
+  local RED='\033[0;31m'   # 红色
+  local NC='\033[0m'
 
-response=$(curl -m 3 -s -o /dev/null -w "%{http_code}" https://code.kubectl.net)
-if [ $response -eq 200 ]; then
-  echo -e "${GREEN}Success: HTTP status code is 200. <https://code.kubectl.net>${NC}"
-  # ROOT_URI=https://code.kubectl.net/devops/build-project/raw/branch/main
-  export ROOT_URI=https://dev.kubectl.net
-else
-  echo -e "${RED}Failed: HTTP status code is not 200, but $response.${NC}"
-  export ROOT_URI=https://gitlab.com/iprt/build-project/-/raw/main
-fi
+  local response=$(curl -m 3 -s -o /dev/null -w "%{http_code}" https://code.kubectl.net)
+  if [ $response -eq 200 ]; then
+    # echo -e "${GREEN}Success: HTTP status code is 200. <https://code.kubectl.net>${NC}"
+    # ROOT_URI=https://code.kubectl.net/devops/build-project/raw/branch/main
+    export ROOT_URI=https://dev.kubectl.net
+  else
+    echo -e "${RED}Failed: HTTP status code is not 200, but $response.${NC}"
+    export ROOT_URI=https://gitlab.com/iprt/build-project/-/raw/main
+  fi
 
-echo -e "${GREEN}ROOT_URI=$ROOT_URI${NC}"
+  echo -e "${GREEN}ROOT_URI=$ROOT_URI${NC}"
+
+}
+getRootUri
