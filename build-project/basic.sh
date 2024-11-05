eb7c42ae3daaf2c06f80ba6cd08e7d0e5a678119
zhenjie zhu <tech@intellij.io>
2024-11-06 00:25:38 +0800

    🛠️ Update ROOT_URI in basic.sh
    Changed ROOT_URI to point to https://dev.kubectl.net instead of https://code.kubectl.net/devops/build-project/raw/branch/main. This ensures the script uses the new URL for fetching resources.

--- build-project/basic.sh
+++ build-project/basic.sh
@@ -7,7 +7,8 @@ NC='\033[0m'
 response=$(curl -m 3 -s -o /dev/null -w "%{http_code}" https://code.kubectl.net)
 if [ $response -eq 200 ]; then
   echo -e "${GREEN}Success: HTTP status code is 200. <https://code.kubectl.net>${NC}"
-  ROOT_URI=https://code.kubectl.net/devops/build-project/raw/branch/main
+  # ROOT_URI=https://code.kubectl.net/devops/build-project/raw/branch/main
+  ROOT_URI=https://dev.kubectl.net
 else
   echo -e "${RED}Failed: HTTP status code is not 200, but $response.${NC}"
   ROOT_URI=https://gitlab.com/iprt/build-project/-/raw/main
