9823f641cb23ac9b8d4ee73e2054865a3f582b59
zhenjie zhu <tech@intellij.io>
2024-11-06 00:44:57 +0800

    🔧 Update shellcheck directive and export ROOT_URI
    Include new shellcheck directive SC2034 to suppress warnings. Change ROOT_URI assignments to export statements for environment variable consistency.

--- build-project/basic.sh
+++ build-project/basic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2086
+# shellcheck disable=SC2086 disable=SC2034
 GREEN='\033[0;32m' # 绿色
 RED='\033[0;31m'   # 红色
 NC='\033[0m'
@@ -8,10 +8,10 @@ response=$(curl -m 3 -s -o /dev/null -w "%{http_code}" https://code.kubectl.net)
 if [ $response -eq 200 ]; then
   echo -e "${GREEN}Success: HTTP status code is 200. <https://code.kubectl.net>${NC}"
   # ROOT_URI=https://code.kubectl.net/devops/build-project/raw/branch/main
-  ROOT_URI=https://dev.kubectl.net
+  export ROOT_URI=https://dev.kubectl.net
 else
   echo -e "${RED}Failed: HTTP status code is not 200, but $response.${NC}"
-  ROOT_URI=https://gitlab.com/iprt/build-project/-/raw/main
+  export ROOT_URI=https://gitlab.com/iprt/build-project/-/raw/main
 fi
 
 echo -e "${GREEN}ROOT_URI=$ROOT_URI${NC}"
